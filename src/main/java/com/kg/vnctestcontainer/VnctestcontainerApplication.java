d10980a1acba276575eb92169a90588780e0dde9
Trainee GSS <trainee@kgisl.com>
2019-01-29 18:17:06 +0530

    first commit

---
+++ src/main/java/com/kg/vnctestcontainer/VnctestcontainerApplication.java
@@ -0,0 +1,14 @@
+package com.kg.vnctestcontainer;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VnctestcontainerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(VnctestcontainerApplication.class, args);
+	}
+
+}
+
